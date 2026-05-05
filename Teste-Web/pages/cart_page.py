6a7a43c21475e4cf629dff4ceb67b5852c6802b2
José Roberto de Sousa Brito Júnior <81085769+Josejr3103@users.noreply.github.com>
2026-05-05 00:40:11 -0300

    Update cart_page.py

--- Teste-Web/pages/cart_page.py
+++ Teste-Web/pages/cart_page.py
@@ -9,7 +9,7 @@ class CartPage(BasePage):
     _CHECKOUT_BUTTON = (By.ID, "checkout")
 
     def is_on_cart_page(self) -> bool:
-        self.wait.until(EC.url_contains("cart.html"))
+        self.wait.until(EC.presence_of_element_located(self._CHECKOUT_BUTTON))
         return True
 
     def get_item_count(self) -> int:
